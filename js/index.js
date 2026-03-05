6a24ae3f275cab30f1ab5ec027f9616205ba2e4c
karenalfaro <kalfarolara@gmail.com>
2026-03-05 11:01:50 +0100

    first gloton commit

---
+++ js/index.js
@@ -0,0 +1,31 @@
+const gloton = document.getElementById('gloton')
+const points = document.getElementById('points')
+
+const btnSubirNivel = document.getElementById('btn-subir-nivel')
+btnSubirNivel.style.display = 'none'
+
+let conteoPuntos = 0
+let puntosNeeded = 10
+let nivel = 1
+
+function alimentar() {
+    if (conteoPuntos < puntosNeeded) {
+        conteoPuntos++
+        points.innerText = conteoPuntos
+        btnSubirNivel.style.display = 'none'
+    } else {
+        btnSubirNivel.style.display = 'block'
+        gloton.classList.remove('ri-emotion-unhappy-fill')
+        gloton.classList.add('ri-emotion-happy-fill')
+    }
+}
+function subirNivel() {
+    nivel++
+    points.innerText = conteoPuntos
+    gloton.style.fontSize = `calc(3rem + ${conteoPuntos}rem)`
+    btnSubirNivel.style.display = 'none'
+    puntosNeeded += 10
+}
+
+
+
